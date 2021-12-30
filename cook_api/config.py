03616576ee7ba07b68d1bbeb2f0e68b3f4133505
gr-vr <goritss@gmail.com>
2021-12-30 16:42:31 +0500

    Clear config.py

--- cook_api/config.py
+++ cook_api/config.py
@@ -9,7 +9,4 @@ class Settings:
         load_dotenv('../.env')
 
     PORT = int(os.environ.get('PORT', 5000))
-    TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
-    APP_URL = os.environ.get('APP_URL')
-    FULL_URL = f'{APP_URL}{TOKEN}'
     DB_URI = os.environ.get('DB_URI')
